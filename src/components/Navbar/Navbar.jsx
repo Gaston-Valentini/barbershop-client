d6aab12056e9d7a6849b2bbc0327a659435eda5e
Gastón Valentini <gastonvalentiniruiz@gmail.com>
2024-03-02 11:30:36 +0100

    Add: Maquetción mobile del navbar.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,25 @@
 import style from "./Navbar.module.css";
 import logo from "../../assets/images/logo-two-yellow.png";
 import { LiaBarsSolid } from "react-icons/lia";
+import { useRef, useState } from "react";
 
 export default function Navbar() {
+    const [isLinksActive, setIsLinksActive] = useState(false);
+    const links = useRef(null);
+
+    const onBars = () => {
+        setIsLinksActive(!isLinksActive);
+    };
+
     return (
         <nav className={style.container}>
             <div className={style.logo}>
-                <img src={logo} />
+                <img src={logo} alt="Logo" />
             </div>
             <div className={style.bars}>
-                <LiaBarsSolid className={style.barsIcon} />
+                <LiaBarsSolid className={style.barsIcon} onClick={onBars} />
             </div>
-            <div className={style.links}>
+            <div className={`${style.links} ${isLinksActive ? style.linksActive : ""}`} ref={links}>
                 <a href="#" className={style.linksLink}>
                     HOME
                 </a>
