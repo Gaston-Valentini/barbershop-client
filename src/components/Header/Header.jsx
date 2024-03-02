085ab3e867ae2f8affb395e6c9893cc8c4c69f0c
Gastón Valentini <gastonvalentiniruiz@gmail.com>
2024-03-02 11:06:58 +0100

    Add: Maquetación dekstop del header.

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import style from "./Header.module.css";
+import logo from "../../assets/images/logo-yellow.png";
+import background from "../../assets/videos/header.mp4";
+
+export default function Header() {
+    return (
+        <section className={style.container}>
+            <div className={style.overlay}></div>
+            <video src={background} muted autoPlay loop />
+            <div className={style.logo}>
+                <img src={logo} />
+            </div>
+        </section>
+    );
+}
